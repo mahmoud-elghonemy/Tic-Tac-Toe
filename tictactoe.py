76c7c3fd571f735c5ed413e27abcf890efb231e0
mahmoudmohamed22 <mahmoudmohamed5555@yahoo.com>
2022-01-26 15:14:52 +0200

    create functions game

---
+++ tictactoe.py
@@ -0,0 +1,66 @@
+"""
+Tic Tac Toe Player
+"""
+
+import math
+from copy import deepcopy
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+  
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    
+
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+   
+
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+   
+                               
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+   
+   
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+
+
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+
+ 
